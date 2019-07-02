d1475b9711cbec8b854772f9315599c0e455016a
Marko Korhonen <marko.korhonen@druid.fi>
2019-07-02 11:40:29 +0300

    Fix sed with Linux

--- tools/make/project/spell.mk
+++ tools/make/project/spell.mk
@@ -4,7 +4,11 @@ spell-init: ## Init Spell project
 	$(call colorecho, "\nInit Spell project...")
 	@rsync -av vendor/druidfi/amazeeio-scripts/dist/ .
 	@sed -i -e 's|mysite|'"${BASENAME}"'|g' .env
+ifeq ($(UNAME_S),Darwin)
 	@sed -i '' '/composer.lock/d' .gitignore
+else
+	@sed -i '/composer.lock/d' .gitignore
+endif
 	@mv README.project.md README.md
 	@rm LICENSE.md .env-e tools/make/project/spell.mk
 	@make self-update
