2f3a1643eacb262b8e5efd9801ccce3c9af7250d
Marko Korhonen <marko.korhonen@druid.fi>
2019-12-05 14:47:19 +0200

    Constant is deprecated

--- public/sites/default/settings.php
+++ public/sites/default/settings.php
@@ -24,5 +24,5 @@ if (isset($_SERVER['WODBY_APP_NAME'])) {
   include '/var/www/conf/wodby.settings.php';
 
   // Override setting from wodby.settings.php.
-  $config_directories[CONFIG_SYNC_DIRECTORY] = '../conf/cmi';
+  $settings['config_sync_directory'] = '../conf/cmi';
 }
