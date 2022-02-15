2a6d42f19e52ce2aa9da8c5990969b53b3b42550
Marko Korhonen <marko.korhonen@druid.fi>
2022-02-15 11:15:40 +0200

    If dev run, set NODE_TLS_REJECT_UNAUTHORIZED to false (using drupal from https docker.so)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
-  images: {
-    domains: [process.env.NEXT_IMAGE_DOMAIN],
-  },
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+  }
+
+  return {
+    images: {
+      domains: [process.env.NEXT_IMAGE_DOMAIN],
+    },
+  }
 }
