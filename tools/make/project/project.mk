738ede6658be8ebfc4f18712b9ec829811900083
Marko Korhonen <marko.korhonen@druid.fi>
2022-02-18 13:52:06 +0200

    Create local oAuth keys

--- tools/make/project/project.mk
+++ tools/make/project/project.mk
@@ -1,4 +1,9 @@
 PHONY += front-run
-front-run:
+front-run: conf/keys
 		$(call step,Start up front application...\n)
 		$(call node_run,run dev)
+
+conf/keys:
+		@mkdir -p conf/keys
+		@openssl genrsa -out conf/keys/private.key 2048
+		@openssl rsa -in conf/keys/private.key -pubout > conf/keys/public.key
