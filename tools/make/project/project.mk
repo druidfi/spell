f09dd4aa0d678ba86969f7b0942006dbc2862c8e
Marko Korhonen <marko.korhonen@druid.fi>
2022-02-18 13:53:16 +0200

    Create oAuth keys

--- tools/make/project/project.mk
+++ tools/make/project/project.mk
@@ -1,9 +1,12 @@
+BUILD_TARGETS := conf/keys
+
 PHONY += front-run
 front-run: conf/keys
 		$(call step,Start up front application...\n)
 		$(call node_run,run dev)
 
 conf/keys:
+		$(call step,Create oAuth keys...\n)
 		@mkdir -p conf/keys
 		@openssl genrsa -out conf/keys/private.key 2048
 		@openssl rsa -in conf/keys/private.key -pubout > conf/keys/public.key
