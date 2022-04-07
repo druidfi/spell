92bed50ed0d0af3ff1b5b9feea75f548d6aa397f
Marko Korhonen <marko.korhonen@druid.fi>
2022-04-07 13:29:23 +0300

    Create nextjs user on make new

--- tools/make/project/project.mk
+++ tools/make/project/project.mk
@@ -1,13 +1,20 @@
 BUILD_TARGETS += conf/keys
+DRUPAL_NEW_TARGETS := up build drush-si next-create-user drush-uli
 
 PHONY += start
 start: conf/keys
-		$(call step,Start up front application...\n)
-		$(call node_run,run dev)
+	$(call step,Start up front application...\n)
+	$(call node_run,run dev)
 
 conf/keys:
-		$(call step,Create oAuth keys...\n)
-		@mkdir -p conf/keys
-		@openssl genrsa -out conf/keys/private.key 2048
-		@openssl rsa -in conf/keys/private.key -pubout > conf/keys/public.key
-		@chmod 600 conf/keys/private.key conf/keys/public.key
+	$(call step,Create oAuth keys...\n)
+	@mkdir -p conf/keys
+	@openssl genrsa -out conf/keys/private.key 2048
+	@openssl rsa -in conf/keys/private.key -pubout > conf/keys/public.key
+	@chmod 600 conf/keys/private.key conf/keys/public.key
+
+PHONY += next-create-user
+next-create-user:
+	$(call step,Create Drupal user for Next.js frontend...\n)
+	$(call drush,user:create nextjs)
+	$(call drush,user:role:add next_site_previewer nextjs)
