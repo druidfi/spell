3ee53e45913c898fae27323be98dd4d58e4cd167
Marko Korhonen <marko.korhonen@druid.fi>
2022-12-20 08:45:53 +0200

    This comes from Omen now

--- public/sites/default/settings.php
+++ public/sites/default/settings.php
@@ -12,9 +12,6 @@
 //
 extract(Druidfi\Omen\Reader::get(get_defined_vars()));
 
-// See https://www.drupal.org/docs/system-requirements/setting-the-mysql-transaction-isolation-level
-$databases['default']['default']['init_commands']['isolation_level'] = 'SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED';
-
 /**
  * Only in Wodby environment. @see https://wodby.com/docs/stacks/drupal/#overriding-settings-from-wodbysettingsphp
  */
